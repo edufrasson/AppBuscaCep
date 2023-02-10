5c776a0cb78f76f2a5034941ad7f1a8da3af0bca
Usuario <Usuario@CPD02-M19>
2023-02-10 14:32:23 -0300

    Adicionar arquivos de projeto.

---
+++ AppBuscaCep/Model/Bairro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AppBuscaCep.Model
+{
+    public class Bairro
+    {
+        public string descricao_bairro { get; set; }
+        
+    }
+}
