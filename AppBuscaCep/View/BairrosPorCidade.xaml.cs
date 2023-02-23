fb168dc8b99496d9e66c6bf031bf7e19c4b493e1
edufrasson <edudomine2016@gmail.com>
2023-02-23 15:24:08 -0300

    atualizando data service

--- AppBuscaCep/View/BairrosPorCidade.xaml.cs
+++ AppBuscaCep/View/BairrosPorCidade.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppBuscaCep.Model;
+using AppBuscaCep.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +15,56 @@ namespace AppBuscaCep.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BairrosPorCidade : ContentPage
     {
+        ObservableCollection<Cidade> lista_cidades = new ObservableCollection<Cidade>();
+        ObservableCollection<Bairro> lista_bairros = new ObservableCollection<Bairro>();
         public BairrosPorCidade()
         {
             InitializeComponent();
+
+            pck_cidade.ItemsSource = lista_cidades;
+            lst_bairros.ItemsSource = lista_bairros;
         }
 
-        private void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
+        private async void pck_estado_SelectedIndexChanged(object sender, EventArgs e)
         {
+            try
+            {
 
+                Picker disparador = sender as Picker;
+
+                string estado = disparador.SelectedItem as string;
+
+                List<Cidade> arr_cidades = await DataService.GetCidadeByEstado(estado);
+                lista_cidades.Clear();
+
+                arr_cidades.ForEach(i => lista_cidades.Add(i));
+
+            }catch(Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "Ok");
+            }
         }
 
         private void pck_cidade_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+            try
+            {
+
+                Picker disparador = sender as Picker;
+
+                string estado = disparador.SelectedItem as int;
+
+                List<Bairro> arr_cidades = await DataService.GetBairrosByIdCidade(estado);
+                lista_cidades.Clear();
+
+                arr_cidades.ForEach(item => lista_bairros.Add(item));
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "Ok");
+            }
         }
     }
 }
